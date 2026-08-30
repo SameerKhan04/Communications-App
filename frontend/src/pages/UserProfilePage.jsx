d081617ce6c621d0e8a4c66bed445616e7f3df0d
skha0730 <skha0730@uni.sydney.edu.au>
2026-08-30 10:20:25 +1000

    fixing double header for friend profile view

--- frontend/src/pages/UserProfilePage.jsx
+++ frontend/src/pages/UserProfilePage.jsx
@@ -103,14 +103,6 @@ function UserProfilePage() {
   // -----------------------------------------
   return (
     <main className="public-profile-page">
-      <header className="public-profile-navbar">
-        <div className="public-profile-logo">
-          <span>CHUM</span>
-          <strong>BUDDY</strong>
-        </div>
-        <span>Profile</span>
-      </header>
-
       <div className="public-profile-container">
         <section className="public-profile-card">
           <div className="public-profile-top">
